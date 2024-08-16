9d7b376ed3e26252bd3724ef03935982cddded64
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-08-16 22:38:25 +0530

    Add files via upload
    Create a function to calculate the area of a rectangle given its width and height.

---
+++ 04.js
@@ -0,0 +1,13 @@
+function area(width,breadth){
+    return width*breadth;
+}
+
+console.log(area(5,6));
+
+// USING ARROW FUNCTION 
+
+let area2 = (width,breadth)=>{
+    return width*breadth;
+}
+
+console.log(area2(5,10));
