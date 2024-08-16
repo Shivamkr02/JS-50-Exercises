51b5f444772b6dae17b3599b789a26c07d663fee
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-08-16 22:37:23 +0530

    Add files via upload
    Print the numbers from 1 to 5 to the console using loop.

---
+++ 03.js
@@ -0,0 +1,21 @@
+// USING FOR LOOP
+
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+
+// USING WHILE LOOP
+
+let i = 1;
+while(i<=5){
+    console.log(i);
+    i++;
+}
+
+// DO WHILE LOOP
+
+let j = 1;
+do{
+    console.log(j);
+    j++;
+}while(j<=5);
