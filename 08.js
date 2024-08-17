a0b7912c182a5d71f37d311fa452295537354548
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-08-17 23:30:05 +0530

    Update 08.js

--- 08.js
+++ 08.js
@@ -2,8 +2,14 @@ let arr = [10,20,30,40,50];
 
 let sum = 0;
 
+// USING FOR IN LOOP
+
 for(i in arr){
     sum += arr[i];
 }
+
+// for(let i=0;i< arr.length;i++){
+//     sum+=arr[i];
+// }
 
 console.log(sum)
